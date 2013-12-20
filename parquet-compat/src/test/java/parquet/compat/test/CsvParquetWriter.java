5fe1f91f4f7d5866efcca7b3b6a8e32b6c3a08f5
Nong Li <nong@cloudera.com>
2013-12-20 14:58:28 -0800

    Shrink block size for csv writer to expose more potential issues.

--- parquet-compat/src/test/java/parquet/compat/test/CsvParquetWriter.java
+++ parquet-compat/src/test/java/parquet/compat/test/CsvParquetWriter.java
@@ -26,16 +26,18 @@ import parquet.hadoop.metadata.CompressionCodecName;
 import parquet.schema.MessageType;
 
 public class CsvParquetWriter extends ParquetWriter<List<String>> {
+  // Reduce the block (row group) size for testing to expose more issues.
+  public static final int TEST_BLOCK_SIZE = 4 * 1024 * 1024;
 
   public CsvParquetWriter(Path file, MessageType schema) throws IOException {
     this(file, schema, false);
   }
-  
+
   public CsvParquetWriter(Path file, MessageType schema, boolean enableDictionary) throws IOException {
     this(file, schema, CompressionCodecName.UNCOMPRESSED, enableDictionary);
   }
-  
+
   public CsvParquetWriter(Path file, MessageType schema, CompressionCodecName codecName, boolean enableDictionary) throws IOException {
-    super(file, (WriteSupport<List<String>>) new CsvWriteSupport(schema), codecName, DEFAULT_BLOCK_SIZE, DEFAULT_PAGE_SIZE, enableDictionary, false);
+    super(file, (WriteSupport<List<String>>) new CsvWriteSupport(schema), codecName, TEST_BLOCK_SIZE, DEFAULT_PAGE_SIZE, enableDictionary, false);
   }
 }
